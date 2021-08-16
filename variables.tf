f51870d07a5c0243ab82a5da03e0f035b7a4e604
Alex Astafyev <alex.astafyev@dualbootpartners.com>
2021-08-16 12:21:00 +0400

    finished Basic part of the course

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type = string
+}
+
+variable "env" {
+  description = "environment"
+  type = string
+}
+
+variable "image" {
+  description = "container image you will run on the instance"
+  type = string
+}
+
+variable "instance_key_file" {
+  description = "public key path"
+  type = string
+}
+
+variable "instance_ami" {
+  description = "AWS AMI"
+  type = string
+}
+
+variable "instance_user" {
+  description = "instance user"
+  type = string
+}
