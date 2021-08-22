d1cf9cb603fea3b504782a339703b287cd4b13be
Alex Astafyev <alex.astafyev@dualbootpartners.com>
2021-08-22 15:04:32 +0000

    switched to Terragrunt

--- common/main.tf
+++
@@ -1,7 +0,0 @@
-module "backend" {
-  source             = "../modules/backend"
-  config_path        = path.module
-  config_name        = "backend.tf"
-  namespace          = "study-infra"
-  copy_config_to     = "../${path.module}"
-}
